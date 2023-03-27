adc080f2754311de7e578165eb1a29092e11ac90
chuckythecat <61181095+chuckythecat@users.noreply.github.com>
2023-03-27 14:21:34 +0800

    Исправлена ошибка, не позволяющая отправлять сообщения из iframe в parent посредством postMessage
    я хз почему это не работает и как я это исправил, добавил флаг, позволяющий включить отладочную информацию

--- src/kiri/frame.js
+++ src/kiri/frame.js
@@ -10,19 +10,37 @@
 // use: kiri.widget
 gapp.register("kiri.frame", [], (root, exports) => {
 
+let debug = false;
+
 // add frame message api listener
 window.addEventListener('message', msg => {
     const { load, kiri, moto } = self;
     const { api, newWidget } = kiri;
     const { conf, event, feature, platform, settings, show } = api;
 
-    if (!feature.frame) return;
+    if (!feature.frame) {
+        if (debug) console.log("frame is not supported");
+        return;
+    }
 
     const { origin, source, target, data } = msg;
 
-    if (source.window === target.window) return;
+    if (debug) {
+        console.warn("frame: new message:");
+        console.log(`frame: origin: ${origin} (${typeof origin})`);
+        // console.log(`source: ${source}`);
+        console.log(`frame: target: ${target.location.href}`);
+        console.log(`frame: source=target: ${source.window === target.window}`);
+        console.log(`frame: data: ${JSON.stringify(data)}`);
+    }
 
-    const send = source.window.postMessage;
+    if (source.window === target.window) {
+        if (debug) console.log("source window is target window");
+        return;
+    }
+
+    let windowparent = window.top;
+    // const send = source.window.postMessage;
 
     if (data.mode) {
         api.mode.set(data.mode.toUpperCase());
@@ -34,14 +52,18 @@ window.addEventListener('message', msg => {
 
     if (data.function) {
         const cb = data.callback ? (output) => {
-            send({event:`${data.function}.done`, data: output});
+            // send({event:`${data.function}.done`, data: output});
+            windowparent.postMessage({event:`${data.function}.done`, data: output}, origin);
         } : undefined;
         api.function[data.function.toLowerCase()](cb);
     }
 
     if (data.event) {
+        if (debug) console.log(`${data.event} subscription from ${origin} received`);
         event.on(data.event, (evd) => {
-            send({event: data.event, data: evd});
+            // send({event: data.event, data: evd}, "*");
+            windowparent.postMessage({event: data.event, data: evd}, origin);
+            if (debug) console.log(`${data.event} sent to ${origin}`);
         });
     }
 
@@ -50,11 +72,15 @@ window.addEventListener('message', msg => {
     }
 
     if (data.get) switch (data.get) {
-        case "mode": send({mode: settings.mode()}); break;
-        case "device": send({device: settings.dev()}); break;
-        case "process": send({process: settings.proc()}); break;
-        default: send({all: settings}); break;
+        // case "mode": send({mode: settings.mode()}); break;
+        // case "device": send({device: settings.dev()}); break;
+        // case "process": send({process: settings.proc()}); break;
+        // default: send({all: settings}); break;
 
+        case "mode": windowparent.postMessage({mode: settings.mode()}, origin); break;
+        case "device": windowparent.postMessage({device: settings.dev()}, origin); break;
+        case "process": windowparent.postMessage({process: settings.proc()}, origin); break;
+        default: windowparent.postMessage({all: settings}, origin); break;
     }
 
     if (data.features) {
@@ -86,7 +112,8 @@ window.addEventListener('message', msg => {
                 if (!bin.buffer) bin = new Float32Array(bin).buffer;
                 new load.STL().parse(bin, vertices => {
                     platform.add(widget = newWidget().loadVertices(vertices));
-                    send({event: "parsed", data: [ widget.id ]});
+                    // send({event: "parsed", data: [ widget.id ]});
+                    windowparent.postMessage({event: "parsed", data: [ widget.id ]}, origin);
                 });
                 break;
             case 'obj':
@@ -102,18 +129,21 @@ window.addEventListener('message', msg => {
                     platform.add(widget = newWidget().loadVertices(svg.toFloat32()));
                     wid.push(widget.id);
                 }
-                send({event: "parsed", data: wid});
+                // send({event: "parsed", data: wid});
+                windowparent.postMessage({event: "parsed", data: wid}, origin);
                 break;
         }
     }
 
     if (data.load) {
         platform.load(data.load, (verts, widget) => {
-            send({event: "loaded", data: [ widget.id ]});
+            // send({event: "loaded", data: [ widget.id ]});
+            windowparent.postMessage({event: "loaded", data: [ widget.id ]}, origin);
         })
     };
 
     if (data.clear) {
+        if (debug) console.log("received command to clear");
         platform.clear();
     }
 
